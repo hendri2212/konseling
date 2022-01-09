3cbde3df54e514e92a51a9b256bf6d1ffae2a1de
hendri2212 <cowok_cool320@yahoo.co.id>
2022-01-09 18:13:38 +0800

    first app

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,28 +1,11 @@
 import logo from './logo.svg';
 import './App.css';
 
-function UserGreeting(props) {
-  return <h1>Welcome back!</h1>;
-}
-
-function GuestGreeting(props) {
-  return <h1>Please sign up.</h1>;
-}
-
-function Greeting(props) {
-  const isLoggedIn = props.isLoggedIn;
-  if (isLoggedIn) {
-    return <UserGreeting />;
-  }
-  return <GuestGreeting />;
-}
-
 function App() {
   return (
     <div className="App">
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
-        <Greeting isLoggedIn={true} />
         <p>
           Edit <code>src/App.js</code> and save to reload.
         </p>
