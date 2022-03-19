a20583f290f66ad0b0e99542b61746b3683e2717
Muhammad Pazrin Andreanor <riansera515@gmail.com>
2022-03-19 23:10:03 +0800

    add: offcanvas daftar soal

--- frontend-user/src/main.js
+++ frontend-user/src/main.js
@@ -1,9 +1,21 @@
 import Vue from 'vue'
 import App from './App.vue'
 import router from './router'
-// import 'bootstrap'
+import bootstrap from 'bootstrap'
 // import 'bootstrap/dist/css/bootstrap.min.css'
 import '@/assets/custom.scss'
+
+// collapse
+// var collapseElementList = [].slice.call(document.querySelectorAll('.collapse'))
+// collapseElementList.map(function (collapseEl) {
+//   return new bootstrap.Collapse(collapseEl)
+// })
+
+// offcanvas
+var offcanvasElementList = [].slice.call(document.querySelectorAll('.offcanvas'))
+offcanvasElementList.map(function (offcanvasEl) {
+  return new bootstrap.Offcanvas(offcanvasEl)
+})
 
 Vue.config.productionTip = false
 
--- frontend-user/src/views/Home.vue
+++ frontend-user/src/views/Home.vue
@@ -1,18 +1,22 @@
 <template>
-  <div class="home container-fluid p-4">
-    <div class="row">
-      <div class="col-sm-3 p-4 d-none d-lg-block border">
+  <div class="home container-sm p-4">
+    <div class="row justify-content-center">
+      <!-- <div class="col-sm-3 p-4 border collapse " id="collapseSoal">
+        <a class="btn btn-close" data-bs-toggle="collapse" href="#collapseSoal" role="button" aria-expanded="false" aria-controls="collapseExample"></a>
+
         <h3>soal list</h3>
 
         <div class="soal row row-cols-5">
-          <div v-for="number in numbers" :key="number" href="#" class="col text-center px-1 pb-1">
-            <a href="#" class="btn btn-outline-primary">{{ number }}</a>
+          <div v-for="number in numbers" :key="number" class="col text-center px-1 pb-1">
+            <a href="#" :class="['btn', number.isAnswered ? answeredClass : defaultClass /*, activePage */ ]">{{ number.num }}</a>
           </div>
         </div>
-      </div>
+      </div> -->
+
+
       <div class="col-lg-9 p-md-4 py-4 border">
         <div class="">
-          <h3>Soal</h3>
+          <h3>Soal 1</h3>
           <span class="my-4">
             <p class="fs-6">Saya merasa belum disiplin dalam beribadah pada Tuhan YME</p>
           </span>          
@@ -35,6 +39,45 @@
           <button type="button" class="btn btn-warning">Kembali</button>
           <button type="button" class="btn btn-primary">Selanjutnya</button>
         </div>
+        <div class="btn-controller py-4 d-flex justify-content-center">
+          <!-- <button type="button" class="btn btn-outline-success">Semua Soal</button> -->
+          <!-- <a class="btn btn-outline-success" data-bs-toggle="collapse" href="#collapseSoal" role="button" aria-expanded="false" aria-controls="collapseExample">
+            Semua Soal
+          </a> -->
+          <a class="btn btn-outline-success" data-bs-toggle="offcanvas" href="#offcanvasSoal" role="button" aria-controls="offcanvasExample">
+            Daftar Soal
+          </a>
+        </div>
+
+        <!-- <div class="collapse testIndex" id="collapseSoal">
+          <div class="card card-body">
+            <h3>soal list</h3>
+
+            <div class="soal row row-cols-5">
+              <div v-for="number in numbers" :key="number" class="col text-center px-1 pb-1">
+                <a href="#" :class="['btn', number.isAnswered ? answeredClass : defaultClass /*, activePage */ ]">{{ number.num }}</a>
+              </div>
+            </div>
+          </div>
+        </div> -->
+
+      </div>
+    </div>
+
+    <div class="offcanvas offcanvas-start" tabindex="-1" id="offcanvasSoal" aria-labelledby="offcanvasSoalLabel">
+      <div class="offcanvas-header">
+        <h5 class="offcanvas-title" id="offcanvasSoalLabel">Daftar Soal</h5>
+        <button type="button" class="btn-close text-reset" data-bs-dismiss="offcanvas" aria-label="Close"></button>
+      </div>
+      <div class="offcanvas-body">
+        <div class="soal row row-cols-5">
+          <div v-for="number in numbers" :key="number" class="col text-center px-1 pb-1">
+            <a href="#" :class="['btn', number.isAnswered ? answeredClass : defaultClass /*, activePage */ ]">{{ number.num }}</a>
+          </div>
+          <div class="col-12 py-4 text-center">
+            <button type="button" class="btn btn-outline-danger">Akhiri Ujian</button>
+          </div>
+        </div>
       </div>
     </div>
   </div>
@@ -44,17 +87,23 @@
 // @ is an alias to /src
 // import HelloWorld from '@/components/HelloWorld.vue'
 
-const numbers = [ 1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12, 13, 14, 15, 16, 17, 18, 19, 20]
+const numbers = [
+  { num: 1, isAnswered: false },
+  { num: 2, isAnswered: true },
+  { num: 3, isAnswered: true },
+  { num: 4, isAnswered: false },
+  { num: 5, isAnswered: false }
+]
 
 export default {
   name: 'Home',
   data() {
     return {
       numbers,
+      answeredClass: 'btn-success',
+      defaultClass: 'btn-outline-primary',
+      // activePage: isSoal === isSoalActive ? 'active' : ''
     }
-  },
-  components: {
-    // HelloWorld
   }
 }
 </script>
