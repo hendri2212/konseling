caf092e819b60722413450bd30c9efbaeeb80b87
ryfazrin <riansera515@gmail.com>
2022-04-10 14:05:32 +0800

    update: change use icons

--- frontend/README.md
+++ frontend/README.md
@@ -8,6 +8,8 @@ release: https://github.com/coreui/coreui-vue/releases/tag/3.2.9
 
 template: https://github.com/coreui/coreui-free-vue-admin-template/releases/tag/3.1.4
 
+coreui icons docs: https://icons.coreui.io/icons/
+
 ## Project setup
 ```
 npm install
--- frontend/src/main.js
+++ frontend/src/main.js
@@ -3,7 +3,8 @@ import App from './App.vue'
 import router from './router'
 import store from './store'
 import CoreuiVue from '@coreui/vue'
-import { iconsSet as icons } from './assets/icons/icons.js'
+// import { iconsSet as icons } from './assets/icons/icons.js'
+import * as icons from '@coreui/icons'
 
 Vue.config.productionTip = false
 Vue.use(CoreuiVue)
